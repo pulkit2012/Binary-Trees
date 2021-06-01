afc7782e87f4bf588ba435de70b3318025ad417e
Pulkit Varshney <pulkitvarshney2012@gmail.com>
2021-06-01 15:47:44 +0530

    vertical order traversal added, all methods

--- src/Tree/VerticalOrderTraversal.java
+++ src/Tree/VerticalOrderTraversal.java
@@ -127,6 +127,8 @@ public class VerticalOrderTraversal {
         root.right = new Node(3);
         root.left.left = new Node(4);
         root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
         verticalOrder(root);
         System.out.println();
         printVerticalOrder(root);
