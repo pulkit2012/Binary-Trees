2025f688c3cdf7420c74d74248f5434c58d64435
Pulkit Varshney <pulkitvarshney2012@gmail.com>
2021-06-03 21:50:14 +0530

    minimum swaps to BST

---
+++ src/Tree/IsSumTree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class IsSumTree {
+    static class Node{
+        int data;
+        Node left,right;
+
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+}
