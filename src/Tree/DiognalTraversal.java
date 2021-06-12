9372cd89320fdfe5c6e58440c56324a91675f4d5
Pulkit Varshney <pulkitvarshney2012@gmail.com>
2021-06-12 18:10:33 +0530

    Practise Class Added

--- src/Tree/DiognalTraversal.java
+++ src/Tree/DiognalTraversal.java
@@ -60,11 +60,15 @@ public class DiognalTraversal {
         }
     }
     public static void main(String[] args) {
-        Node root = new Node(1);
-        root.left = new Node(2);
-        root.right = new Node(3);
-        root.left.left = new Node(4);
-        root.left.right = new Node(5);
+        Node root = new Node(8);
+        root.left = new Node(3);
+        root.right = new Node(10);
+        root.right.right = new Node(14);
+        root.right.right.left = new Node(13);
+        root.left.left = new Node(1);
+        root.left.right = new Node(6);
+        root.left.right.right = new Node(7);
+        root.left.right.left = new Node(4);
         diagonalTraversal(root);
         System.out.println();
         diagonalTraversal2(root);
